584c2548927ed3774167a5c4be43a2fe7bedd5ba
wilmot <wilmot@fias.uni-frankfurt.de>
2019-09-25 11:58:27 +0200

    dummy

--- src/cluster.sh
+++ src/cluster.sh
@@ -8,4 +8,4 @@
 
 ##SBATCH --exclude springtalk
 
-srun python3 training.py --sequence-length 100 -bs 200 -ema 0.999 -nw 20 -np 1
+srun python3 training.py --sequence-length 100 -bs 200 -ema 0.999 -nw 20 -np 1 -fe 20000 -df 0.9 --description gamma_0.9
