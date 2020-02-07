832aab7495f475899e1d4d4005967c23606cac62
Unknown <charles.wilmot2@hotmail.fr>
2020-02-07 14:04:07 +0100

    script improvements

--- src/copy_events_files.py
+++ src/copy_events_files.py
@@ -2,6 +2,7 @@ if __name__ == "__main__":
     import os
     import argparse
     from tempfile import TemporaryDirectory
+    from asynchronous import get_available_port
 
 
     parser = argparse.ArgumentParser()
@@ -30,10 +31,18 @@ if __name__ == "__main__":
     prefix = commonprefix(basenames)
 
     grouppath = "../grouped_events_files/{}/".format(prefix)
-    os.mkdir(grouppath)
+    if not os.path.exists(grouppath):
+        os.mkdir(grouppath)
     for path, basename in zip(args.path, basenames):
         runname = basename.replace(prefix, "")
-        os.mkdir(grouppath + "/" + runname)
-        filename = [f for f in os.listdir(path + "/log") if f.find("tfevents") != -1][0]
-        os.link(path + '/log/' + filename, grouppath + "/" + runname + "/" + filename)
-    os.system("tensorboard --logdir {}".format(grouppath))
+        filename = [f for f in os.listdir(path + "/log") if f.find("tfevents") != -1]
+        if len(filename):
+            filename = filename[0]
+            if not os.path.exists(grouppath + "/" + runname):
+                os.mkdir(grouppath + "/" + runname)
+            if not os.path.exists(grouppath + "/" + runname + "/" + filename):
+                os.link(path + '/log/' + filename, grouppath + "/" + runname + "/" + filename)
+        else:
+            print(path, "has not started")
+    port = get_available_port(6006)
+    os.system("tensorboard --logdir {} --port {}".format(grouppath, port))
