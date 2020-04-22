5a3595c6690f90ef558afce6cc2aff657e740e67
Unknown <charles.wilmot2@hotmail.fr>
2020-04-22 16:23:24 +0200

    Dynamically build buffer (relative to the multiple timescales thing)

--- src/replay_buffer.py
+++ src/replay_buffer.py
@@ -5,23 +5,39 @@ from copy import deepcopy
 class Buffer:
     def __init__(self, size):
         self._size = size
-        self._current_size = 0
-        self._data = []
+        self._current_insert_index = 0
+        self._buffer_is_built = False
 
-    def incorporate_multiple(self, iterator):
-        for element in iterator:
-            self.incorporate(element)
+    def incorporate(self, transition_0, transition_1):
+        if not self._buffer_is_built:
+            self.build_buffer(transition_0)
+        index = self.get_insertion_index()
+        self.incorporate_at(index, transition_0, transition_1)
 
-    def incorporate(self, element):
-        if self._current_size < self._size:
-            self._data.append(deepcopy(element))
-            self._current_size += 1
+    def build_buffer(self, transition):
+        dtype_description = []
+        dtype_description += [(key, np.float32, val.shape) for key, val in transition.items()]
+        dtype_description += [(key.replace("0", "1"), np.float32, val.shape) for key, val in transition.items()]
+        self._dtype = np.dtype(dtype_description)
+        self._buffer = np.zeros(self._size, dtype=self._dtype)
+        self._buffer_is_built = True
+
+    def get_insertion_index(self):
+        if self._current_insert_index < self._size:
+            self._current_insert_index += 1
+            return self._current_insert_index - 1
         else:
-            self._data[np.random.randint(0, self._current_size)] = deepcopy(element)
+            return np.random.randint(0, self._size)
+
+    def incorporate_at(self, index, transition_0, transition_1):
+        for key_0, val_0 in transition_0.items():
+            self._buffer[index][key_0] = val_0
+        for key_1, val_1 in transition_1.items():
+            self._buffer[index][key_1.replace("0", "1")] = val_1
 
     def batch(self, size):
-        if size >= self._current_size:
-            return self._data
+        if size >= self._current_insert_index:
+            return self._buffer[:self._current_insert_index]
         else:
-            indices = np.random.choice(self._current_size, size, replace=False)
-            return [self._data[i] for i in indices]
+            index_start = np.random.randint(0, self._current_insert_index - size)
+            return self._buffer[index_start:index_start + size]
